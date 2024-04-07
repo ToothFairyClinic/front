cdce3d3c7a78f5715b8df7ae370c387fc8829704
Sin-god <illya.komarnickii1@gmail.com>
2024-04-07 09:58:23 +0300

    upload video to cloudinary

--- src/modules/main/components/slide/slide.component.tsx
+++ src/modules/main/components/slide/slide.component.tsx
@@ -1,20 +1,29 @@
 import { FC } from "react";
 // @ts-ignore
 import videoBg from "@app/assets/videos/main_page_video.mp4";
+import { AdvancedVideo } from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen";
 import CatImage from "@app/assets/images/cat.png";
+import { useCloudinaryImage } from "@app/common/hooks/use-cloudinary-image.hook";
 interface SlideProps {}
 
 export const Slide: FC<SlideProps> = ({}) => {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "dc7d3byxg",
+    },
+  });
+  const myVideo = cld.video("videos/cjqq4jqlqe6vqia4xyot");
+
   return (
     <div className="relative h-165 lg:h-screen flex justify-center items-end  pb-15">
-      <video
+      <AdvancedVideo
         className="absolute inset-0 w-full h-full object-cover"
-        src={videoBg}
+        cldVid={myVideo}
         autoPlay
         loop
         muted
-        disablePictureInPicture
-      ></video>
+      ></AdvancedVideo>
       <div className="absolute text-center text-darkGray z-10 max-w-150 bg-paleOlive/75 dark:bg-darkGray/75 dark:text-white py-12 px-8 lg:rounded-full transition  delay-150">
         <h1 className=" font-normal text-6xl mb-8">Зубна Фея</h1>
         <p className="font-normal text-3xl">
