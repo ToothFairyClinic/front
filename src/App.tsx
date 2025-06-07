9282c64e1c3c4c3125ab50cc6cfb8d10ca621137
Sin-god <illya.komarnickii1@gmail.com>
2025-06-07 19:00:10 +0300

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { isLoggedInReactive } from "./modules/auth/store/reactive-vars";
 import { Header } from "./common/components/header/header.component";
 import { Routes, Route } from "react-router-dom";
@@ -9,8 +10,29 @@ import { ServicePage } from "./modules/services/pages/services.page";
 import { ReviewsPage } from "./modules/reviews/pages/reviews.page";
 import { OurWorkPage } from "./modules/our-works/page/our-work.page";
 import { ContactPage } from "./modules/contacts/pages/contacts.page";
+import ReactGA from "react-ga4";
+import { config } from "./core/config";
 
 export const App = () => {
+  const location = useLocation();
+  const GA_ID = config.GA_MEASUREMENT_ID;
+
+  useEffect(() => {
+    if (GA_ID)
+      // ініціалізація GA
+      ReactGA.initialize(GA_ID);
+  }, [GA_ID]);
+
+  useEffect(() => {
+    if (GA_ID) {
+      ReactGA.send({
+        hitType: "pageview",
+        page: location.pathname + location.search,
+        title: document.title,
+      });
+    }
+  }, [location, GA_ID]);
+
   useEffect(() => {
     const token = localStorage.getItem("jwt");
     isLoggedInReactive(Boolean(token));
--- src/core/config.ts
+++ src/core/config.ts
@@ -1,3 +1,4 @@
 export const config = {
   hasuraEndpoint: process.env.REACT_APP_HASURA_ENDPOINT,
+  GA_MEASUREMENT_ID: process.env.REACT_APP_GA_ID,
 };
