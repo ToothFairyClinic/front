f53cd10830418a15ce9fab93dc51871ba239bdab
Illya Komarnitskyi <illya.komarnickii1@gmail.com>
2026-05-09 17:09:03 +0300

    add preload to fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import 'react-toastify/dist/ReactToastify.css';
 import "./index.css";
 import { App } from "@app/App";
 import { ApolloProvider } from "@apollo/client";
